ba7ee7201ef24bc534a16a26fb7ec939c6af435c
Erick Rivera <yomellamoerick.771@gmail.com>
2024-12-19 19:21:52 -0300

    primer commit

---
+++ src/main/java/com/pawpengaga/SpringSecurityPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.pawpengaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityPruebaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pawpengaga/SpringSecurityPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawpengaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
