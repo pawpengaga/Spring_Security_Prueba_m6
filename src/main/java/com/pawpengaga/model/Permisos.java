2576942b36d03676613063fc5c3e6b293b85c655
Erick Rivera <yomellamoerick.771@gmail.com>
2024-12-19 20:27:45 -0300

    entidad de permisos lista

--- src/main/java/com/pawpengaga/model/Permisos.java
+++ src/main/java/com/pawpengaga/model/Permisos.java
@@ -1,7 +1,29 @@
 package com.pawpengaga.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder // Otro metodo de lombok
+@Data
+@Entity
+@Table(name = "permisos")
 public class Permisos {
 
-  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(unique = true, nullable = false)
+  private String name;
 
 }
